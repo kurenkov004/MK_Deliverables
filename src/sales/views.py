69dba848bcf3e9379b3d9176ebbadf3474a4b738
kurenkov004 <mary.kurenkov@gmail.com>
2024-09-06 14:46:02 -0400

    ex2.4 file upload

--- src/sales/views.py
+++ src/sales/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
 
-# Create your views here.
+# Create your views here
+def home(request):
+  #returns the template file available & the request itself
+  return render(request, 'sales/home.html')
